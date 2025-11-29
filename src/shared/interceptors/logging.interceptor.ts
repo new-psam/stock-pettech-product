42cef089eb0659f3df71b0ef4dbbb8084e779415
new-psam <marcelino.patricio@hotmail.com>
2025-11-29 06:35:38 -0300

    feat(main): criado o prometheus service

--- src/shared/interceptors/logging.interceptor.ts
+++ src/shared/interceptors/logging.interceptor.ts
@@ -1,21 +1,32 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
 import { Observable, tap } from "rxjs";
+import { PrometheusService } from "../services/prometheus.service";
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
+    constructor(private readonly prometheusService: PrometheusService,){}
     intercept(
         context: ExecutionContext, 
         next: CallHandler<any>
     ): Observable<any> | Promise<Observable<any>> {
         const request = context.switchToHttp().getRequest();
 
-        console.log(request.headers);
+      
 
         const now = Date.now();
 
         return next
                 .handle()
-                .pipe(tap(() => console.log(`After...${Date.now()- now}ms `)));
+                .pipe(tap(() => {
+                    console.log('Route =>', request.route.path);
+                    console.log(`After...${Date.now()- now}ms `);
+
+                    const duration = Date.now() - now;
+                    this.prometheusService.sendMetrics
+                        .labels(request.route.path)
+                        .observe(duration / 1000);
+                }),
+            );
     }
     
 }
